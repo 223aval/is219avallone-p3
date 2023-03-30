989da0f6086b37ccece7bd907ec1293929e002f2
223aval <223pavallone@frhsd.com>
2023-03-30 12:28:50 -0400

    up to slide 23

--- site.js
+++ site.js
@@ -20,9 +20,7 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
-var title = "IMDB + Val's top 8 movies";
-var owner = "Val";
-var github = "http://www.github.com/223aval/is219avallone-p3";
+
 
 const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
@@ -34,15 +32,75 @@ const vue_app = Vue.createApp({
       },
       data() {
         return {
+            
             // This holds your movies.json data.
             movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            title: "IMDB + Val's top 8 movies",
+            owner: "Val",
+            github: "http://www.github.com/223aval/is219avallone-p3"
          
       }
     },
       methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
+            getMonthText(dateArray){
+                  if(dateArray[1] == 1){
+                        return "January " + dateArray[2] + ", " + dateArray[0]
+                  }
+                  if(dateArray[1] == 2){
+                        return "February " + dateArray[2] + ", " + dateArray[0]
+                  }
+                  if(dateArray[1] == 3){
+                        return "March " + dateArray[2] + ", " + dateArray[0]
+                  }
+                  if(dateArray[1] == 4){
+                        return "April " + dateArray[2] + ", " + dateArray[0]
+                  }
+                  if(dateArray[1] == 5){
+                        return "May " + dateArray[2] + ", " + dateArray[0]
+                  }
+                  if(dateArray[1] == 6){
+                        return "June " + dateArray[2] + ", " + dateArray[0]
+                  }
+                  if(dateArray[1] == 7){
+                        return "July " + dateArray[2] + ", " + dateArray[0]
+                  }
+                  if(dateArray[1] == 8){
+                        return "August " + dateArray[2] + ", " + dateArray[0]
+                  }
+                  if(dateArray[1] == 9){
+                        return "September " + dateArray[2] + ", " + dateArray[0]
+                  }
+                  if(dateArray[1] == 10){
+                        return "October " + dateArray[2] + ", " + dateArray[0]
+                  }
+                  if(dateArray[1] == 11){
+                        return "November " + dateArray[2] + ", " + dateArray[0]
+                  }
+                  if(dateArray[1] == 12){
+                        return "December " + dateArray[2] + ", " + dateArray[0]
+                  }                 
+            },
+
+            like(index){
+                  this.movies[index].likes++
+            },
+            dislike(index){
+                  this.movies[index].dislikes++
+            },
+            posterClick(index){
+                  let length = this.movies[index].posters.length
+                  
+                  this.movies[index].posterindex++
+                  if(this.movies[index].posterindex >= length){
+                        this.movies[index].posterindex = 0
+                  }
+            },
+            timeText(minutes){
+                  let hr = Math.trunc(minutes/60)
+                  let min = minutes - 60*hr
+                  return hr + "h " + min + "m"
+            }
+      },
 })
 
 vue_app.mount("#vue_app")
